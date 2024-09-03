12ed5d055d88100051bd2d8ad7d57422d81e5430
Madhav1101 <162007024+Madhav1101@users.noreply.github.com>
2024-09-03 19:28:17 +0530

    baysianAlgo

---
+++ baysian.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+vx = np.array([4,9,8,24,21,14,12,20,36,8,12,5,29])
+vy = np.array([45,88,77,145,71,82,88,92,125,78,98,97,90])
+plt.scatter(vx, vy)
+plt.show()
